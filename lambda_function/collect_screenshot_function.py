b12650ba50509c3561a9754761e926c5ffe986b0
HIN0815 <43261589+HIN0815@users.noreply.github.com>
2018-10-08 11:57:14 +0800

    Add files via upload

--- lambda_function/collect_screenshot_function.py
+++ lambda_function/collect_screenshot_function.py
@@ -1,14 +1,13 @@
-
 import boto3
 import json
 import os
 import datetime
-
+import  requests
 
 print('Loading function')
+# Get the service client
 s3 = boto3.client('s3')
 apigateway = boto3.client('apigateway')
-
 
 def respond(err, res=None):
     return {
@@ -16,9 +15,8 @@ def respond(err, res=None):
         'body': err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
-        }
+        },
     }
-
 
 def lambda_handler(event, context):
     apiKey = apigateway.get_api_key(apiKey=event["requestContext"]["identity"]["apiKeyId"],includeValue=True)
@@ -28,7 +26,7 @@ def lambda_handler(event, context):
     filename = now.strftime("Screenshot_%M_%S.jpg")
     
     Bucket= os.environ['StudentLabDataBucket']
-    Key=f"screenshot/{partition}/id={apiKey['name']}/{filename}"
+    Key=f"screenshot_stream/{partition}/id={apiKey['name']}/{filename}"
     fields = {"acl": "public-read"}
 
 
